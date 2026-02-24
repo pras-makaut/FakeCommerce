108e5374770c15cb9fb01dd627fd9127782f9f95
Prashant Ranjan <prashantranjan@Prashants-MacBook-Air.local>
2026-02-24 16:08:25 +0530

    added jpa auditing created at updated at using jpa auditiing mechanism

--- src/main/java/com/example/FakeCommerce/Schema/BaseSchema.java
+++ src/main/java/com/example/FakeCommerce/Schema/BaseSchema.java
@@ -3,12 +3,29 @@ package com.example.FakeCommerce.Schema;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseSchema {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @CreatedDate
+    @Column(name = "created_at",nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
